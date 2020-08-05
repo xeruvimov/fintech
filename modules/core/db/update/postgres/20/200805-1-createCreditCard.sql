c344eafb638d8e813b4261a729bf433fd3b3d611
Sergey Kheruvimov <kheruvimov@haulmont.com>
2020-08-05 16:24:40 +0400

    fix

---
+++ modules/core/db/update/postgres/20/200805-1-createCreditCard.sql
@@ -0,0 +1,32 @@
+create table FINTECH_CREDIT_CARD (
+    ID uuid,
+    VERSION integer not null,
+    CREATE_TS timestamp,
+    CREATED_BY varchar(50),
+    UPDATE_TS timestamp,
+    UPDATED_BY varchar(50),
+    DELETE_TS timestamp,
+    DELETED_BY varchar(50),
+    --
+    FIRST_NAME varchar(255),
+    MIDDLE_NAME varchar(255),
+    LAST_NAME varchar(255),
+    BIRTHDAY date,
+    PHONE_NUMBER varchar(255),
+    EMAIL varchar(255),
+    ADDRESS varchar(255),
+    PASSPORT_NUMBER varchar(6),
+    PASSPORT_SERIAL varchar(4),
+    PASSPORT_DATE date,
+    PASSPORT_ORGANISATION varchar(255),
+    WORK_PLACE varchar(255),
+    WORK_EXPERIENCE varchar(255),
+    MONTHLY_INCOME varchar(255),
+    EMPLOYER_ADDRESS varchar(255),
+    EMPLOYER_PHONE_NUMBER varchar(255),
+    MARITAL_STATUS varchar(255),
+    USER_MESSAGER_ID uuid,
+    STATUS varchar(50),
+    --
+    primary key (ID)
+);
