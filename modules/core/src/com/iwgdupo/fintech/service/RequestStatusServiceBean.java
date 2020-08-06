0eb1f36555418ff2cf5c15bec23188409477a1fc
Xeruvimov <xeruvimov@gmail.com>
2020-08-06 22:59:25 +0400

    fix status get

--- modules/core/src/com/iwgdupo/fintech/service/RequestStatusServiceBean.java
+++ modules/core/src/com/iwgdupo/fintech/service/RequestStatusServiceBean.java
@@ -2,6 +2,7 @@ package com.iwgdupo.fintech.service;
 
 import com.haulmont.cuba.core.Persistence;
 import com.haulmont.cuba.core.Transaction;
+import com.iwgdupo.fintech.entity.CreditCard;
 import com.iwgdupo.fintech.entity.DebitCard;
 import com.iwgdupo.fintech.entity.MinimalRequestDTO;
 import org.springframework.stereotype.Service;
@@ -21,13 +22,14 @@ public class RequestStatusServiceBean implements RequestStatusService {
     @Override
     public List<MinimalRequestDTO> getActiveRequestsByUserId(String id) {
         List<MinimalRequestDTO> result = new ArrayList<>(getActiveDebitCard(id));
+        result.addAll(getActiveCreditCard(id));
         return result;
     }
 
     @Override
     @Deprecated
     public MinimalRequestDTO getRequestById(String id, String type) {
-        try(Transaction transaction = persistence.createTransaction()) {
+        try (Transaction transaction = persistence.createTransaction()) {
             switch (type) {
                 case DebitCard.NAME:
                     return mapDebitCardOnDTO(Objects.requireNonNull(
@@ -36,6 +38,17 @@ public class RequestStatusServiceBean implements RequestStatusService {
             transaction.commit();
         }
         return null;
+    }
+
+    private List<MinimalRequestDTO> getActiveCreditCard(String id) {
+        try (Transaction transaction = persistence.createTransaction()) {
+            List<CreditCard> debitCardList = persistence.getEntityManager().createQuery("select dc from fintech_CreditCard dc " +
+                    "where dc.userMessager.telegramId = :id", CreditCard.class)
+                    .setParameter("id", id)
+                    .getResultList();
+            transaction.commit();
+            return debitCardList.stream().map(this::mapCreditCardOnDTO).collect(Collectors.toList());
+        }
     }
 
     private List<MinimalRequestDTO> getActiveDebitCard(String id) {
@@ -47,6 +60,14 @@ public class RequestStatusServiceBean implements RequestStatusService {
             transaction.commit();
             return debitCardList.stream().map(this::mapDebitCardOnDTO).collect(Collectors.toList());
         }
+    }
+
+    private MinimalRequestDTO mapCreditCardOnDTO(CreditCard creditCard) {
+        MinimalRequestDTO minimalRequestDTO = new MinimalRequestDTO();
+        minimalRequestDTO.setId(creditCard.getId());
+        minimalRequestDTO.setType(DebitCard.NAME);
+        minimalRequestDTO.setStatus(creditCard.getStatus());
+        return minimalRequestDTO;
     }
 
     private MinimalRequestDTO mapDebitCardOnDTO(DebitCard debitCard) {
